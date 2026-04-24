6c937b92da3f0a60fa9d5863e12342441fca7f28
Ratish149 <ratish.shakya149@gmail.com>
2026-04-24 16:58:20 +0545

    fix tag filter using multiple tags

--- lead/filters.py
+++ lead/filters.py
@@ -4,7 +4,14 @@ from .models import ActivityTimeline, Lead
 
 
 class LeadFilter(django_filters.FilterSet):
-    tag = django_filters.CharFilter(field_name="tags__name", lookup_expr="icontains")
+    tag = django_filters.CharFilter(method="filter_by_tags")
+
+    def filter_by_tags(self, queryset, name, value):
+        if not value:
+            return queryset
+        tag_names = [t.strip() for t in value.split(",")]
+        return queryset.filter(tags__name__in=tag_names).distinct()
+
     min_rating = django_filters.NumberFilter(field_name="rating", lookup_expr="gte")
     max_rating = django_filters.NumberFilter(field_name="rating", lookup_expr="lte")
 
